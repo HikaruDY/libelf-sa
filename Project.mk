3ade00ad321cc613831e3b3e759bd42b6713eb91
Hikaru Yamatohimemiya <Hikaru@oss.xprj.org>
2024-12-09 02:59:44 +0900

    Update configure scripts

--- Project.mk
+++ Project.mk
@@ -1,1 +1,9 @@
 
+OUTPUT_DIR = bin
+
+CFLAGS_INTERNAL = -I$(SROOT) -I$(SROOT)/lib -I$(SROOT)/src/elf 
+CFLAGS_INTERNAL += -Wno-implicit-function-declaration -Wno-int-conversion
+
+INSTALL_HEADER_FILES = $(addprefix src/elf/, elf.h libelf.h gelf.h nlist.h)
+
+SUBDIRS_LIB = elf
